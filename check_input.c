074f4e5e997a81dc4083be84f2e589b6dd79eee5
magoosse <magoosse@student.42.fr>
2025-07-01 18:20:23 +0200

    update_tester

--- check_input.c
+++ check_input.c
@@ -6,7 +6,7 @@
 /*   By: magoosse <magoosse@student.42.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/27 18:18:33 by magoosse          #+#    #+#             */
-/*   Updated: 2025/07/01 17:28:19 by magoosse         ###   ########.fr       */
+/*   Updated: 2025/07/01 18:15:36 by magoosse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -61,15 +61,14 @@ int	check_input(char *input, t_sh *shell)
 			{
 				if (ft_isalnum(input[i]) || input[i] == '$')
 					valid = 1;
-				if (input[i] == '\0')
-				{
-					printf_fd(STDERR,
-						" unexpected EOF while looking for matching `%c'",
-						quote);
-					shell->exit_status = 2;
-					return (ERROR);
-				}
 				i++;
+			}
+			if (input[i] == '\0')
+			{
+				printf_fd(STDERR,
+					" unexpected EOF while looking for matching `%c'", quote);
+				shell->exit_status = 2;
+				return (ERROR);
 			}
 		}
 		if (input[i] == '|')
