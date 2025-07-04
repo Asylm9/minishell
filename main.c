b5b715e3be505248c13faa43034ad2ce608a6766
magoosse <magoosse@student.42.fr>
2025-07-04 14:28:06 +0200

    update_sig_heredoc_main

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: magoosse <magoosse@student.42.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/04 16:10:00 by magoosse          #+#    #+#             */
-/*   Updated: 2025/07/04 14:26:25 by magoosse         ###   ########.fr       */
+/*   Updated: 2025/07/04 14:26:50 by magoosse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -46,8 +46,7 @@ void	handle_here_sig(int sig)
 	g_sig = sig;
 	rl_replace_line("", 0);
 	rl_on_new_line();
-	rl_redisplay();
-	rl_done = 1;
+	ioctl(STDIN_FILENO, TIOCSTI, "\n");
 }
 
 void	handle_sigint(int sig)
