e4f4a4f8b2d1822d8e96cdd6d6f1312a614a6c1f
asylum9 <agaland@student.s19.be>
2025-07-05 17:53:12 +0200

    cd: expansion ~

--- srcs/builtins/cd.c
+++ srcs/builtins/cd.c
@@ -13,7 +13,9 @@ static char	*set_new_path(char **args, t_sh *shell)
 		new_path = get_envl_var("HOME", shell->envl);
 	else if (argc == 2)
 	{
-		if (args[1][0] == '-')
+		if (args[1][0] == '~')
+			new_path = get_envl_var("HOME", shell->envl);
+		else if (args[1][0] == '-')
 			new_path = get_envl_var("OLDPWD", shell->envl);
 		else
 			new_path = ft_strdup(args[1]);
