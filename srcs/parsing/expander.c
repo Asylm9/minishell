4efaed539ff3b307c0bca3223c6524f019c56b78
magoosse <magoosse@student.42.fr>
2025-07-15 20:51:23 +0200

    update_expand

--- srcs/parsing/expander.c
+++ srcs/parsing/expander.c
@@ -6,7 +6,7 @@
 /*   By: magoosse <magoosse@student.42.be>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/10 16:09:51 by magoosse          #+#    #+#             */
-/*   Updated: 2025/07/15 18:10:04 by magoosse         ###   ########.fr       */
+/*   Updated: 2025/07/15 19:10:42 by magoosse         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,14 +21,9 @@ char	*trim_quotes(char *input)
 	{
 		match_quotes(input, &exp.end, &exp.quote);
 		exp.tmp = ft_substr(input, exp.start, exp.end - exp.start);
-		if (exp.result)
-		{
-			exp.buffer = ft_strdup(exp.result);
-			free(exp.result);
-			exp.result = ft_fstrjoin(&exp.buffer, &exp.tmp, 3);
-		}
-		else
-			exp.result = exp.tmp;
+		exp.buffer = ft_strdup(exp.result);
+		free(exp.result);
+		exp.result = ft_fstrjoin(&exp.buffer, &exp.tmp, 3);
 		if (input[exp.end] != '\0')
 			exp.end++;
 		exp.start = exp.end;
