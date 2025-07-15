ac320085158e88e317c26ee1713e40b5531f8004
magoosse <magoosse@student.42.fr>
2025-07-15 22:01:36 +0200

    update_exit

--- srcs/builtins/exit.c
+++ srcs/builtins/exit.c
@@ -20,6 +20,42 @@ bool	is_numeric(char *arg)
 	return (true);
 }
 
+int	valid_exit(char *code)
+{
+	int		i;
+	int		sign;
+	long	result;
+	long	checker;
+
+	i = 0;
+	sign = 1;
+	result = 0;
+	checker = 0;
+	while (code[i] == ' ' || (code[i] >= 9 && code[i] <= 13))
+		i++;
+	if (code[i] == '-' || code[i] == '+')
+		if (code[i++] == '-')
+			sign = -1;
+	while (code[i] > 47 && code[i] < 58)
+	{
+		result = (result * 10) + (code[i] - '0');
+		if (checker > result && ((result < 0 && sign == 1) || (result > 0
+					&& sign == -1)))
+			return (1);
+		checker = result;
+		i++;
+	}
+	return (0);
+}
+
+void	exit_arg_error(t_ast *ast)
+{
+	printf_fd(STDERR,
+				"minishell: exit:\
+				%s: numeric argument required\n",
+				ast->cmd->args[1]);
+}
+
 int	builtin_exit(t_ast *ast, t_sh *shell)
 {
 	printf_fd(1, "exit\n");
@@ -27,10 +63,7 @@ int	builtin_exit(t_ast *ast, t_sh *shell)
 	{
 		if (!is_numeric(ast->cmd->args[1]))
 		{
-			printf_fd(STDERR,
-						"minishell: exit:\
-				%s: numeric argument required\n",
-						ast->cmd->args[1]);
+			exit_arg_error(ast);
 			shell->exit_status = BUILTIN_ERR;
 		}
 		else if (ast->cmd->args[2])
@@ -38,8 +71,13 @@ int	builtin_exit(t_ast *ast, t_sh *shell)
 			printf_fd(STDERR, "minishell: exit: too many arguments\n");
 			return (ERROR);
 		}
-		else
+		else if (valid_exit(ast->cmd->args[1]) == 0)
 			shell->exit_status = ft_atoi(ast->cmd->args[1]);
+		else
+		{
+			exit_arg_error(ast);
+			shell->exit_status = 2;
+		}
 	}
 	clean_exit(shell, ast);
 	return (SUCCESS);
