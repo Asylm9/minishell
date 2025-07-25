ac6c9cf86a9958c9b376d41117f4cfc944924b5a
agaland <agaland@student.s19.be>
2025-07-25 02:43:37 +0200

    malloc wrappers family

--- srcs/main_utils.c
+++ srcs/main_utils.c
@@ -6,7 +6,7 @@
 /*   By: agaland <agaland@student.s19.be>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/16 15:03:39 by magoosse          #+#    #+#             */
-/*   Updated: 2025/07/24 19:42:19 by agaland          ###   ########.fr       */
+/*   Updated: 2025/07/25 02:39:51 by agaland          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,40 @@ void	cleanup_exit(t_sh *shell, t_ast *ast)
 {
 	cleanup_shell(shell, ast);
 	exit(shell->exit_status);
+}
+
+void	*x_malloc(int size, t_sh *shell, t_ast *ast, void *var_to_free)
+{
+	void	*temp;
+
+	temp = malloc(size);
+	if (!temp)
+	{
+		if (var_to_free)
+			free(var_to_free);
+		malloc_exit(shell, ast);
+	}
+	return (temp);
+}
+
+char	*x_strjoin(const char *s1, const char *s2 , t_sh *shell, t_ast *ast)
+{
+	void	*temp;
+
+	temp = ft_strjoin(s1, s2);
+	if (!temp)
+		malloc_exit(shell, ast);
+	return (temp);
+}
+
+char	*x_strdup(const char *s1, t_sh *shell, t_ast *ast)
+{
+	void	*temp;
+
+	temp = ft_strdup(s1);
+	if (!temp)
+		malloc_exit(shell, ast);
+	return (temp);
 }
 
 void	malloc_exit(t_sh *shell, t_ast *ast)
